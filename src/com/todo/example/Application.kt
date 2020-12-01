ee9c51456b9f365ce158252adaa9e0059f2b6e0f
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2020-12-01 23:18:19 +0900

    mock化のユニットテスト

--- src/com/todo/example/Application.kt
+++ src/com/todo/example/Application.kt
@@ -2,7 +2,6 @@ package com.todo.example
 
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.todo.example.factory.DatabaseFactory
-import com.todo.example.service.TodoServiceImpl
 import com.todo.example.service.serviceModule
 import com.todo.example.web.todos
 import io.ktor.application.*
@@ -10,6 +9,7 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
 import io.ktor.routing.Routing
 import io.ktor.util.KtorExperimentalAPI
+import org.koin.core.module.Module
 import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
@@ -17,7 +17,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args
 @KtorExperimentalAPI
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module(testing: Boolean = false, mockModule: Module? = null) {
 
     install(ContentNegotiation) {
         jackson {
@@ -26,7 +26,8 @@ fun Application.module(testing: Boolean = false) {
     }
 
     install(Koin) {
-        modules(serviceModule)
+        if(testing && mockModule != null) modules(mockModule)
+        else modules(serviceModule)
     }
 
     DatabaseFactory.init()
--- test/com/todo/example/web/TodoControllerTest.kt
+++ test/com/todo/example/web/TodoControllerTest.kt
@@ -1,31 +1,69 @@
 package com.todo.example.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.todo.example.model.NewTodo
+import com.todo.example.model.Todo
 import com.todo.example.module
+import com.todo.example.service.TodoService
+import com.todo.example.service.TodoServiceImpl
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.handleRequest
 import org.junit.Before
 import org.junit.Test
+import org.koin.core.module.Module
+import org.koin.dsl.module
 import kotlin.test.assertEquals
 
 class TodoControllerTest {
-    private lateinit var engine: TestApplicationEngine
+    private lateinit var mapper: ObjectMapper
 
     @Before
     fun before() {
-        engine = TestApplicationEngine().apply {
-            start(wait = false)
-            application.module()
-        }
+        mapper = jacksonObjectMapper()
+        mapper.enable(SerializationFeature.INDENT_OUTPUT)
     }
 
     @Test
     fun タスク一覧を呼び出すと200が変えること() {
+        val todos = listOf(Todo(1, "test1"), Todo(2, "test2"))
+        val mockModule: Module = module {
+            single{ MockTodoService(todos) as TodoService }
+        }
+
+        val engine: TestApplicationEngine = TestApplicationEngine().apply {
+            start(wait = false)
+            application.module(true, mockModule)
+        }
+
         with(engine) {
             handleRequest(HttpMethod.Get, "/todos").response.apply {
                 assertEquals(HttpStatusCode.OK, status())
+                assertEquals(mapper.writeValueAsString(todos), content)
             }
         }
+    }
+}
+
+class MockTodoService(
+    private val todos: List<Todo>
+): TodoService {
+    override suspend fun getAllTodos(): List<Todo> {
+        return todos
+    }
+    override suspend fun getTodo(id: Int): Todo? {
+        return null
+    }
+    override suspend fun addTodo(todo: NewTodo): Todo {
+        return Todo(3, "test3")
+    }
+    override suspend fun updateTodo(todo: NewTodo): Todo? {
+        return null
+    }
+    override suspend fun deleteTodo(id: Int): Boolean {
+        return true
     }
 }
